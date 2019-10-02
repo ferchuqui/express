ec0540e6bc7e5ee317334471240ebba04b750216
ferchuqui <luisfernandofarji@hotmail.com>
2019-10-02 13:27:43 -0300

    Using pug as template engine

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-const express = require('express');
+ 
+const express = require("express");
+const path = require("path");
 const app = express();
-const expressJsx = require('./express-jsx');
+const productsRouter = require('./routes/products');
 
-app.engine("jsx", expressJsx);
-app.set('views', './views');
-app.set('view engine', 'jsx');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.get('/', function(req, res) {
-  res.render('index', {Hello: 'Hola', World: 'Mundo'});
-});
+app.use("/products", productsRouter);
 
-const server = app.listen(8000, function(){
-  console.log(`Listening in http://localhost:${server.address().port}`) ;
+const server = app.listen(8000, function() {
+  console.log(`Listening http://localhost:${server.address().port}`);
 });
